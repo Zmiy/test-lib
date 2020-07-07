368d3a26d63aa28d4c1e17ab9eefc816da6f1b04
Zmiy <Zmiy@users.noreply.github.com>
2020-07-07 23:11:00 +0300

    add chanjes for deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAS7a4VOCLwKRbivvYG2nKKLpQewP42_ZE',
+    authDomain: 'test-lib-37174.firebaseapp.com',
+    databaseURL: 'https://test-lib-37174.firebaseio.com',
+    projectId: 'test-lib-37174',
+    storageBucket: 'test-lib-37174.appspot.com',
+    messagingSenderId: '1046319670473',
+    appId: '1:1046319670473:web:557a5c2aaff42237335918',
+    measurementId: 'G-L48VK03HY6'
+  }
 };
